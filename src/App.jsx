1be947361c9585c3a88022dda476fe6ebfad6d80
nuttawit007 <nuttawit.ph@gmail.com>
2026-03-19 22:21:15 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline container mx-auto mt-10">
+        Client Side Rendering (CSR) with React and Vite Project
+      </h1>
+    </>
+  )
+}
+
+export default App
